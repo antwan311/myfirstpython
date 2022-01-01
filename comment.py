ba64ac219b79c08b70d4c57e98dd821d64fa5405
antwan311 <antwan311@outlook.com>
2022-01-01 17:32:30 -0500

    quiz answers

---
+++ comment.py
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------------
+# Name:        comment.py
+# Purpose:this program prints hello antwan and after that it prints the answer of 1+2
+#
+# Author:      antwan
+#
+# Created:     01/01/2022
+# Copyright:   (c) antwa 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+print ("Hello Antwan");
+print (1+2);
