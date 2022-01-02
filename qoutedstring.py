2bfc4c9691005f13cd2349d57db5e53b9a984218
antwan311 <antwan311@outlook.com>
2022-01-02 17:28:44 -0500

    qoutedstring

---
+++ qoutedstring.py
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------------
+# Name:  qoutedstrig.py
+# Purpose:
+#
+# Author:      antwa
+#
+# Created:     02/01/2022
+# Copyright:   (c) antwa 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+print('''"Wow", Alexa said, "Antwan's bike is nice!"
+''');
