d160355da7c41e9053381b051b3196493dab9575
antwan311 <antwan311@outlook.com>
2022-01-05 18:51:37 -0500

    2.8 2.9

---
+++ contecanation.py
@@ -0,0 +1,29 @@
+#-------------------------------------------------------------------------------
+# Name:        concatenation
+# Purpose:
+#
+# Author:      antwa
+#
+# Created:     05/01/2022
+# Copyright:   (c) antwa 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+f = "Antwan";
+l = " Loiseau";
+n1 = "1";
+n2 = "4";
+n3 = "6";
+n4 = "5";
+one = 5;
+two = 6;
+three = 23;
+four = 32;
+five = 78;
+six = 54;
+print (f+l);
+print(n1 + n2);
+print(n3 + n4);
+print (one*two);
+print (three * four);
+print (five*six);
---
+++ input.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:       input.py
+# Purpose:
+#
+# Author:      antwa
+#
+# Created:     05/01/2022
+# Copyright:   (c) antwa 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+n = input ("enter your name:");
+a = input("enter your age:");
+print("My names's",n, ", and I am", int (a)
+,"years old.");
